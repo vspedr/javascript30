87dacf08c68a979f2c8475397137b15be0959234
Victor <vspedr@users.noreply.github.com>
2018-04-04 23:10:05 -0300

    Remove stray console.log()

--- src/components/DrumKit/DrumKit.js
+++ src/components/DrumKit/DrumKit.js
@@ -40,7 +40,6 @@ class DrumKit extends Component {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    console.log(key);
     if (key) key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
